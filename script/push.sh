bc2c71f4ec9ca7481be564c2ea112281059d7e2d
niuliangtao@youzan.com <niuliangtao@youzan.com>
2020-02-28 11:39:51 +0800

    add

--- script/push.sh
+++ script/push.sh
@@ -4,4 +4,4 @@ git commit -a -m "add"
 #git push origin --force
 git push origin master --force
 
-#pip install git+https://github.com/notechats/notetool.git
+pip install git+https://github.com/notechats/notetool.git
