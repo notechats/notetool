24487ee1cb96a6951c3a62424ebbdd08a6c57c29
niuliangtao@youzan.com <niuliangtao@youzan.com>
2020-08-20 10:54:25 +0800

    add

--- notetool/tool/log.py
+++ notetool/tool/log.py
@@ -1,10 +1,17 @@
 import logging
 
+default_level = logging.INFO
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(process)d-%(processName)s - %(filename)s-%(funcName)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
 
-def log(name=None, level=logging.INFO):
+def set_default_level(level):
+    global default_level
+    default_level = level
+
+
+def log(name=None, level=default_level):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
